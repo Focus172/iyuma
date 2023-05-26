52adf759724a69720c9bc5fec27e888dd88127e2
Focus172 <evan.stokdyk@gmail.com>
2023-05-25 17:30:06 -0700

    fix and new computer

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,7 +24,7 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Tijuana";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
